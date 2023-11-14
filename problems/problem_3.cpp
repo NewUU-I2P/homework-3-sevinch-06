8dd7286b3f3e816b4b5433ab77d91e603ae45518
sevinch-06 <147800350+sevinch-06@users.noreply.github.com>
2023-11-14 21:15:15 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,37 +1,25 @@
-#include <iostream>
 #include <string>
-
-int main() {
-    char sex;
-    float height;
-
-    std::cout << "Enter sex (M/F) and height (in meters): ";
-    std::cin >> sex >> height;
-
-    std::string description;
-
-    if (sex == 'M') {
-        if (height < 1.70) {
-            description = "Short";
-        } else if (height < 1.85) {
-            description = "Normal";
-        } else {
-            description = "Tall";
-        }
-    } else if (sex == 'F') {
-        if (height < 1.60) {
-            description = "Short";
-        } else if (height < 1.75) {
-            description = "Normal";
-        } else {
-            description = "Tall";
-        }
-    } else {
-        std::cout << "Invalid input for sex. Please enter 'M' or 'F'." << std::endl;
-        return 1;  // Exit with an error code
-    }
-
-    std::cout << "Output: " << description << std::endl;
-
-    return 0;
+std:: string problemSolution3(float height, char S){
+std:: string result;
+switch (S) {
+        case 'M':
+            if (height < 1.70) {
+                result = "Short";
+            } else if (height>=1.70&&height < 1.85) {
+                result = "Normal";
+            } else {
+                result = "Tall";
+            }
+            break;
+        case 'F':
+            if (height<1.60) {
+                result = "Short";
+            }else if(height>1.60&&height <1.75) {
+                  result = "Normal";
+            }else {
+                result = "Tall";
+            }
+            break;
+   }      
+    return result;
 }
