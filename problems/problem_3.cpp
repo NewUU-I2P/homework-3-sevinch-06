ad891fdb8c2e2f46afb44754104d41eda53e0b55
sevinch-06 <147800350+sevinch-06@users.noreply.github.com>
2023-11-14 10:59:25 -0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,11 +1,37 @@
+#include <iostream>
 #include <string>
 
-std::string problemSolution3(float height, char S) {
-   std::string height1;
-    printf("Enter the prices of three products: ");
-    scanf("%f %f %f", &price1, &price2, &price3);
- float maxPrice = (price1 > price2) ? ((price1 > price3) ? price1 : price3) : ((price2 > price3) ? price2 : price3);
-printf("The maximum number is %.2f\n", maxPrice);
+int main() {
+    char sex;
+    float height;
+
+    std::cout << "Enter sex (M/F) and height (in meters): ";
+    std::cin >> sex >> height;
+
+    std::string description;
+
+    if (sex == 'M') {
+        if (height < 1.70) {
+            description = "Short";
+        } else if (height < 1.85) {
+            description = "Normal";
+        } else {
+            description = "Tall";
+        }
+    } else if (sex == 'F') {
+        if (height < 1.60) {
+            description = "Short";
+        } else if (height < 1.75) {
+            description = "Normal";
+        } else {
+            description = "Tall";
+        }
+    } else {
+        std::cout << "Invalid input for sex. Please enter 'M' or 'F'." << std::endl;
+        return 1;  // Exit with an error code
+    }
+
+    std::cout << "Output: " << description << std::endl;
 
     return 0;
 }
