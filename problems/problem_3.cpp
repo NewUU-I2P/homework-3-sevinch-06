8ce872a1fd5b78fc1c2e0f0eaced04ca3e4a8c6a
sevinch-06 <147800350+sevinch-06@users.noreply.github.com>
2023-11-14 20:35:38 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,22 +2,10 @@
 
 std::string problemSolution3(float height, char S) {
    std::string height1;
-    if(S == 'M'){
-        if(height < 1.70)
-            height1 = "Short";
-        if(height >= 1.70 && height < 1.85)
-            height1 = "Normal";
-        if(height >= 1.85)
-            height1 = "Tall";
-    }
-    if(S == 'F'){
-        if(height < 1.60)
-            height1 = "Short";
-        if(height >= 1.60 && height < 1.75)
-            height1 = "Normal";
-        if(height >= 1.75)
-            height1 = "Tall";
-    }
-}
-  return height1;
+    printf("Enter the prices of three products: ");
+    scanf("%f %f %f", &price1, &price2, &price3);
+ float maxPrice = (price1 > price2) ? ((price1 > price3) ? price1 : price3) : ((price2 > price3) ? price2 : price3);
+printf("The maximum number is %.2f\n", maxPrice);
+
+    return 0;
 }
