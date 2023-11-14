aa8af267315aed04731611dccdd57c0d34af4861
sevinch-06 <147800350+sevinch-06@users.noreply.github.com>
2023-11-14 20:14:57 +0500

    Update problem_5.cpp

--- problems/problem_5.cpp
+++ problems/problem_5.cpp
@@ -1,6 +1,25 @@
 float problemSolution5(float x, float y, char operation) {
    float result;
-   // write your code here
+     switch(operation) {
+        case '+':
+            result = x + y;
+            break;
+
+        case '-':
+            result = x - y;
+            break;
+
+        case '*':
+            result = x * y;
+            break;
+
+        case '/':
+            result = x / y;
+            break;
+
+        default:
+            break;
+    }
 
    return result;
 }
