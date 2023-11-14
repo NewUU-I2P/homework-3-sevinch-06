ba30a9eb7eb020a666d4699a0844c2968898b382
sevinch-06 <147800350+sevinch-06@users.noreply.github.com>
2023-11-14 20:14:10 +0500

    Update problem_4.cpp

--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -2,8 +2,33 @@
 #include <sstream>
 
 std::string problemSolution4(const std::string &macAddress) {
-    // write your code here
+    std::string result;
+    int counter=0;
+    for ( int i=0; i<18; i+=3){
+        if (macAddress[i]=='F' && macAddress[i+1]=='F'){
+            counter+=2;
+            break;
+        }
+        if (counter==12){
+            result = "broadcast";
+            break;
+        }
+    }
+     if (isdigit(macAddress[1]) == true) {
+         if (macAddress[1]%2 == 0) {
+             result = "Unicast";
+         }
+         else {
+             result = "Multicast";
+         }
+     }
+     else if (int(macAddress[1]+1)%2==0){
+         result = "Unicast";
+     }
+     else {
+         result = "Multicast";
+     }
 
-    // make use of control flow statements
-    // return result;
+}
+ return result;
 }
