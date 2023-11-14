55969aab04d62fdacebb7ca4c23537e53572348d
sevinch-06 <147800350+sevinch-06@users.noreply.github.com>
2023-11-14 20:10:40 +0500

    Update problem_1.cpp

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,6 +1,22 @@
-void problemSolution1(float consumed_water) {
+float problemSolution1(float consumed_water) {
     float cost;
-    // write your code here
+    cost = 13;
+    if(consumed_water <= 30){
+        cost = cost + (consumed_water * 0.4);
+    }else{
+        cost = cost + (30 * 0.4);
+        if(consumed_water <= 50){
+            cost = cost + ((consumed_water - 30) * 0.12);
+        }else{
+            cost = cost + (20 * 0.12);
+            if(consumed_water <= 60){
+                cost = cost + ((consumed_water - 50) * 1.4);
+            }else{
+                cost = cost + (10 * 1.4);
+                cost = cost + ((consumed_water - 60) * 1.5);
+            }
+        }
+    }
 
     return cost;
 }
