8b16d4e2e7ce4c3e8fe127b30cc2556dfdfa0d06
sevinch-06 <147800350+sevinch-06@users.noreply.github.com>
2023-11-14 20:13:03 +0500

    Update problem_3.cpp

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,8 +1,23 @@
 #include <string>
 
 std::string problemSolution3(float height, char S) {
-    // write your code here
-
-    // use return to return your result
-    // make use of control flow statements
+   std::string height1;
+    if(S == 'M'){
+        if(height < 1.70)
+            height1 = "Short";
+        if(height >= 1.70 && height < 1.85)
+            height1 = "Normal";
+        if(height >= 1.85)
+            height1 = "Tall";
+    }
+    if(S == 'F'){
+        if(height < 1.60)
+            height1 = "Short";
+        if(height >= 1.60 && height < 1.75)
+            height1 = "Normal";
+        if(height >= 1.75)
+            height1 = "Tall";
+    }
+}
+  return height1;
 }
